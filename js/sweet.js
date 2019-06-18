79f30a1447facf9d35d1ed636dccde871d4ed39c
York <apple095161@gmail.com>
2019-06-18 20:21:17 +0800

    update

---
+++ js/sweet.js
@@ -0,0 +1,12 @@
+/*  $(document).ready(function() {
+    $('.nav-item').click(function(){
+       $('.nav-item').removeClass('active');
+    })
+ }); */
+  
+
+  /* $('.nav-item').click(function(){
+      $(".nav-item").removeClass("active");
+      $(".nav-item").addClass("active");
+  })
+ */
